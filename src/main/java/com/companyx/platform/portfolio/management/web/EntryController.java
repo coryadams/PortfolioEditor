4525aeaf76c801ba2d6600ab310ee317b7edf43d
cadams <coadams@paypal.com>
2015-04-21 19:40:49 -0500

    Small edits

--- src/main/java/com/companyx/platform/portfolio/management/web/EntryController.java
+++ src/main/java/com/companyx/platform/portfolio/management/web/EntryController.java
@@ -1,5 +1,9 @@
 package com.companyx.platform.portfolio.management.web;
 
+import com.companyx.platform.portfolio.management.service.BondService;
+import com.companyx.platform.portfolio.management.service.EquityService;
+import com.companyx.platform.portfolio.management.service.FutureService;
+import com.companyx.platform.portfolio.management.service.OptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,8 +18,24 @@ import java.util.Set;
 @Controller
 public class EntryController {
 
+    @Autowired
+    EquityService equityService;
+
+    @Autowired
+    OptionService optionService;
+
+    @Autowired
+    BondService bondService;
+
+    @Autowired
+    FutureService futureService;
+
     @RequestMapping("/")
     public String home(Model model, HttpServletRequest httpServletRequest, HttpSession httpSession) {
+        model.addAttribute("totalEquities", equityService.findAll().size());
+        model.addAttribute("totalOptions", optionService.findAll().size());
+        model.addAttribute("totalBonds", bondService.findAll().size());
+        model.addAttribute("totalFutures", futureService.findAll().size());
         return "index";
     }
 
